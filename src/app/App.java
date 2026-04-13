c043e02a321812203520fd8d4f81ea5883db2da0
isjustlucas <vastokinha100@gmail.com>
2026-04-13 18:33:15 -0300

    Create files

---
+++ src/app/App.java
@@ -0,0 +1,53 @@
+package app;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+import java.util.Scanner;
+
+import entity.Employees;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter full file path: ");
+        String path = sc.next();
+
+        try (BufferedReader br = new BufferedReader(new FileReader(path))) {
+            List<Employees> list = new ArrayList<>();
+
+            String line = br.readLine();
+            while (line != null) {
+                String[] fields = line.split(",");
+                list.add(new Employees(fields[0], fields[1], Double.parseDouble(fields[2])));
+                line = br.readLine();
+            }
+            System.out.print("Enter salary: ");
+            double salary = sc.nextDouble();
+
+            List<String> emails = list.stream()
+                    .filter(x -> x.getSalary() > salary)
+                    .map(x -> x.getEmail())
+                    .sorted()
+                    .toList();
+
+            System.out.println("Email of people whose salary is more than " + String.format("%.2f", salary) + ":");
+            emails.forEach(System.out::println);
+
+            double sum = list.stream()
+                    .filter(x -> x.getName().charAt(0) == 'M')
+                    .map(x -> x.getSalary())
+                    .reduce(0.0, (x, y) -> x + y);
+
+            System.out.println("Sum of salary of people whose name starts with 'M': " + String.format("%.2f", sum));
+        } catch (IOException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+        sc.close();
+    }
+}
